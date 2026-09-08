d8e622c1708dd0ef795e38ae8ab4cc5be3f7cd4f
Pieterpannenkoek <99074807@mydavinci.nl>
2026-09-08 11:09:29 +0200

    editted file
    added faculteiten

--- test 1.py
+++ test 1.py
@@ -2,4 +2,12 @@ def faculteit(n):
     totaal = 1
     while n > 1:
         totaal *= n
-    return n 
+    return n
+
+if __name__ is "__main__":
+    print(faculteit(0))
+    print(faculteit(1))
+    print(faculteit(2))
+    print(faculteit(3))
+    print(faculteit(4))
+    print(faculteit(5))
